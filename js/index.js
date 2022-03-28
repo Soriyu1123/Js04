9be4a2da399a2339eb95cb63dd1b41e95048ccbd
Master Soriyu <89372061+Soriyu1123@users.noreply.github.com>
2022-03-28 10:41:56 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,34 +1,34 @@
-
-// step 1: Adding a variables for the movement of the card
+// STEP 1: ADDING A VARIABLE FOR THE MOVEMENT OF THE CARD
 var container = document.querySelector('.container');
 var card = document.querySelector('.card');
 
-// step 2: moving effect the selected vars under step 1
-container.addEventListener('mousemove', function(event){
-    var xAxis = (window.innerWidth / 2 - event.pageX) / 20; // rotating the container element into x axis
-    var yAxis = (window.innerWidth / 2 - event.pageY) / 20; // rotating the container element into x axis
+// STEP 2: MOVING  EFFECT THE SELECTED VARS UNDER STEP 1
+container.addEventListener('mousemove', function(event) {
+    var xAxis = (window.innerWidth / 2 - event.pageX) / 20;
+    // ROTATING THE CONTAINER ELEMENT INTO X AXIS
+    var yAxis = (window.innerWidth / 2 - event.pageY) / 20;
+    // ROTATING THE CONTAINER ELEMENT INTO Y AXIS
 
     card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+
 });
 // `` - back tick - for ES6 EcmaScript 6
 // older js version - rotateY(xAxis + 'deg') + rotateX(yAxis + 'deg');
 
-// step 3: Revoving the animation in
-container.addEventListener('mouseleave', function(){
+// step 3: REVOVING THE ANIMATAON IN
+container.addEventListener('mouseleave', function() {
     card.style.transform = "all 0.5s ease";
     card.style.transform = "rotateX(0deg) rotateY(0deg)";
 });
 
-// Step 4: var for card details 3d effects (like floating)
+// step 4: VAR for card details 3d effects (like floating)
 var title = document.querySelector('.title');
-var social = document.querySelector('.social');
-var profile = document.querySelector('.profile');
-var photo = document.querySelector('.photo img');
+var title = document.querySelector('.social');
+var title = document.querySelector('.profile');
+var title = document.querySelector('.photo img');
 
-
-// Step 5: Applying the floating effect on the card container
+// step 5: applying the floating effect on the card container
 title.style.transform = "translateZ(100px)";
 social.style.transform = "translateZ(100px)";
 profile.style.transform = "translateZ(100px)";
 photo.style.transform = "translateZ(100px)";
-
